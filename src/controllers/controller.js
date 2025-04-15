f2e44479f9079b9dea580f0a7da8295f080f1398
paulocamfe <camilloferreirapaulo@gmail.com>
2025-04-15 14:18:15 -0300

    criação de controlles e routers

---
+++ src/controllers/controller.js
@@ -0,0 +1,85 @@
+const produtoController = {
+    create: async (req, res) => {
+        try {
+            const { nome, email, senha } = req.body;
+
+            if (!nome || !email || !senha) {
+                return res.status(400).json({
+                    msg: "Faltou enviar os campos"
+                })  
+            }
+
+            // Salvamos no banco...
+
+            // 201 - Created
+
+            return res.status(201).json({
+                msg: "User criado com sucesso"
+            })
+ 
+ 
+        } catch (error) {
+            return res.status(500).json({
+                msg: "Deu um erro no sistema"
+            })
+        }
+    },
+    delete: async (req, res) => {
+        try {
+            const { id } = req.params;
+
+            if (!id) {
+                return res.status(400).json({
+                    msg:"Pendente o id"
+                })
+            }
+
+            // Deletar o User
+            
+        } catch (error) {
+           return res.status(500).json({
+            msg: "Ocorreu um erro ao criar o User"
+           })
+        }
+    },
+    update: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const { nome, email, senha } = req.body;
+ 
+            if (!id || !nome || !email || !senha) {
+                return res.status(400).json({
+                    msg: "Pendente campos"
+                })
+            }
+ 
+            // Atualizar User...
+ 
+            return res.status(200).json({
+                msg: "User atualizado com sucesso"
+            })
+        } catch (error) {
+            return res.status(500).json({
+                msg: "Ocorreu um erro interno"
+            })
+        }
+    },
+    getAll: async (req, res) => {
+        try {
+            // Busca dos User 
+
+            return res.status(200).json({
+                msg: "User encontrados",
+                users: []
+            })
+            
+        } catch (error) {
+            return res.status(500).json({
+                msg: "Ocorreu um erro interno"
+            })
+            
+        }
+    }
+}
+ 
+module.exports = userController;
---
+++ src/routers/routers.js
@@ -0,0 +1,14 @@
+const  { Router } = require("express");
+const userController = require("../controllers/produtoController");
+ 
+const router = Router();
+ 
+router.get("/", (req, res) => userController.getAll(req, res));
+ 
+router.post("/", (req, res) => userController.create(req, res));
+ 
+router.delete("/:id", (req, res) => userController.delete(req, res));
+ 
+router.put("/:id", (req, res) => userController.update(req, res));
+
+module.exports = router;
